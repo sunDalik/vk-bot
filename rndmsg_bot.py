e4b37c1746db4ba5989eced2c1b737a0dc8b4bc2
sunDalik <dali1999@mail.ru>
2019-10-18 22:08:30 +0300

    Cut max generated sentence length

--- rndmsg_bot.py
+++ rndmsg_bot.py
@@ -59,9 +59,9 @@ def rndmsg_mode(msg_list, mentions):
                     # generate messages with markov chain
                     if shitpost_re.search(e.text.lower()):
                         if shitpost_re.search(e.text.lower()).group(1) is not None:
-                            message = model.make_sentence_with_start(shitpost_re.search(e.text.lower()).group(1), strict=False, max_words=20)
+                            message = model.make_sentence_with_start(shitpost_re.search(e.text.lower()).group(1), strict=False, max_words=15)
                         else:
-                            message = model.make_short_sentence(140)
+                            message = model.make_short_sentence(115)
                         send_message(e.peer_id, message)
 
                     # if mention + what do you think of X then reply with random message about X
