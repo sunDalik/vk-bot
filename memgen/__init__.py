8717ddf7d0d05e766e74ef96f7cabdf29a724de7
johnny-keker <giomail.iv@gmail.com>
2019-10-20 22:58:13 +0300

    added multiline meme logic

--- memgen/__init__.py
+++ memgen/__init__.py
@@ -33,10 +33,23 @@ def make_meme_with_top_bottom(top_lines, bottom_lines, filename, out_filename):
 
     fontsizes = []
     for line in top_lines:
-        fontsizes.append(get_fontsize(image_size, line))
+        font_size = get_fontsize(image_size, line)
+        if (font_size < 20):        # tune this const
+            top_lines = split_lines(line)
+            fontsizes.append(get_fontsize(image_size, top_lines[0]))
+            fontsizes.append(get_fontsize(image_size, top_lines[1]))
+        else:
+            fontsizes.append(font_size)
     for line in bottom_lines:
-        fontsizes.append(get_fontsize(image_size, line))
+        font_size = get_fontsize(image_size, line)
+        if (font_size < 20):        # tune this const
+            bottom_lines = split_lines(line, True)
+            fontsizes.append(get_fontsize(image_size, bottom_lines[0]))
+            fontsizes.append(get_fontsize(image_size, bottom_lines[1]))
+        else:
+            fontsizes.append(font_size)
     font_size = min(fontsizes)
+
     font = ImageFont.truetype(font_file, font_size)
 
     line_num = 0
@@ -68,25 +81,42 @@ def make_meme_with_top_bottom(top_lines, bottom_lines, filename, out_filename):
 
     img.save(out_filename)
 
+def split_lines(line, reversed=False):
+    letter_count = len(line)
+    words = line.split(' ')
+    if reversed:
+        words = words[::-1]
+    words_count = len(words)
+    l_count = 0
+    line1 = ""
+    for i, word in enumerate(words):
+        l_count += len(word)
+        line1 = f"{word} " + line1 if reversed else line1 + f"{word} "
+        if l_count >= letter_count / 2:
+            if reversed:
+                return [' '.join(words[i+1:][::-1]), line1[:-1]]
+            else:
+                return [line1[:-1], ' '.join(words[i+1:])]
+
 def get_fontsize(image_size, text):
-	# find biggest font size that works
-	font_size = int(image_size[1]/10)
-	font = ImageFont.truetype(font_file, font_size)
-	text_size = font.getsize(text)
-	while text_size[0] > image_size[0]-20:
-		font_size = font_size - 1
-		font = ImageFont.truetype(font_file, font_size)
-		text_size = font.getsize(text)
-	return font_size
+        # find biggest font size that works
+        font_size = int(image_size[1]/10)
+        font = ImageFont.truetype(font_file, font_size)
+        text_size = font.getsize(text)
+        while text_size[0] > image_size[0]-20:
+                font_size = font_size - 1
+                font = ImageFont.truetype(font_file, font_size)
+                text_size = font.getsize(text)
+        return font_size
 
 def get_text_position_top(image_size, line_num, text_size):
-	# find top centered position for top text
-	textPositionX = (image_size[0]/2) - (text_size[0]/2)
-	textPositionY = (text_size[1] + 2) * line_num
-	return (textPositionX, textPositionY)
+        # find top centered position for top text
+        textPositionX = (image_size[0]/2) - (text_size[0]/2)
+        textPositionY = (text_size[1] + 2) * line_num
+        return (textPositionX, textPositionY)
 
 def get_text_position_bottom(image_size, line_num, text_size):
-	# find bottom centered position for bottom text
-	textPositionX = (image_size[0]/2) - (text_size[0]/2)
-	textPositionY = image_size[1] - (text_size[1] + 2) * line_num
-	return (textPositionX, textPositionY)
+        # find bottom centered position for bottom text
+        textPositionX = (image_size[0]/2) - (text_size[0]/2)
+        textPositionY = image_size[1] - (text_size[1] + 2) * line_num
+        return (textPositionX, textPositionY)
